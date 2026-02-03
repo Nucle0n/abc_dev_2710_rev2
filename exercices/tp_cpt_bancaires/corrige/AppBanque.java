b92740a2fd91ba3340f4623c39e459cfffcd6224
Nucleon <thibaud.guinchard@gmail.com>
2026-02-03 10:24:05 +0100

    bank

---
+++ exercices/tp_cpt_bancaires/corrige/AppBanque.java
@@ -0,0 +1,15 @@
+public class AppBanque {
+    public static void main(String[] args) {
+        
+        final int nbComptes;
+        final Compte[] listComptes;
+
+        Banque b = new Banque();
+        b.Init();
+
+        b.afficherComptes();
+
+
+
+    }
+}
---
+++ exercices/tp_cpt_bancaires/corrige/Banque.java
@@ -0,0 +1,42 @@
+public class Banque {
+
+    private int nbComptes;
+    private Compte[] listComptes;
+
+    public Banque(){
+
+        nbComptes = 0;
+        listComptes = new Compte[20];
+        
+    }
+        
+    public void Init(){
+        Compte c1 = new Compte("Toto", 5200, -500);
+        Compte c2 = new Compte("Titi", 10200, -500);
+        Compte c3 = new Compte("Dupond", 3200, 0);
+        Compte c4 = new Compte("Durand", 12200, -2000);
+        Compte c5 = new Compte("Dubois", 8000, -200);
+        Compte c6 = new Compte("Duval", 2200, 0);
+
+        ajouteCompte(c1);
+        ajouteCompte(c2);
+        ajouteCompte(c3);
+        ajouteCompte(c4);
+        ajouteCompte(c5);
+        ajouteCompte(c6);
+
+    }
+
+    private void ajouteCompte(Compte unCompte){
+        listComptes[nbComptes++] = unCompte;
+    }
+
+    public static void afficherComptes(Compte[] _listComptes){
+        System.out.print("\n");
+        for (Compte nCompte : _listComptes) {
+            System.out.print(nCompte);
+        }
+
+    }
+    
+}
