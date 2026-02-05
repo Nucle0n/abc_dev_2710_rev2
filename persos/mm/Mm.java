16717c229010da31591f52c0af9964444d7f0958
Nucleon <thibaud.guinchard@gmail.com>
2026-02-05 16:57:13 +0100

    Master Mind ++

--- persos/mm/Mm.java
+++ persos/mm/Mm.java
@@ -1,7 +1,26 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Mm {
+
+    public enum Verif
+        {
+            BIEN_PLACE(VERT),
+            MAL_PLACE(JAUNE),
+            ABSENT(GRIS);
+
+            private final String color;
+
+            private Verif(String color)
+            {
+                this.color = color;
+            }
+            public String getColor()
+            {
+                return this.color;
+            }
+        }
     public static void main(String[] args)
     {
         Scanner sc   = new Scanner(System.in);
@@ -10,11 +29,31 @@ public class Mm {
         int[]   tab  = new int[4],
                 jTab = new int[4];
 
+        String  JAUNE   = "\033[38;2;255;255;0m",
+                VERT    = "\033[38;2;0;255;0m",
+                GRIS    = "\033[38;2;50;50;50m",
+                ENDCOL  = "\033[m";
+
+        
+
+        // boolean[] compareTab = new boolean[4];
+        // boolean[] compareTab = {false,false,false,false};
+
+        Verif[] compareTab = new Verif[4];
+
         String     prop;
 
         for (int i = 0; i < tab.length; i++)
         {
             tab[i] = rng.nextInt(0, 10);
+        }
+
+        System.out.print("\n");
+        
+        System.out.print("\n\033[4;1mDebug :\033[m\n" + "Combinaison secrète :\t");
+        for (int i = 0; i < tab.length; i++)
+        {
+            System.out.print(tab[i]+" ");
         }
 
         System.out.print("\n");
@@ -27,17 +66,45 @@ public class Mm {
         }
 
 
-        System.out.print("\n\033[4;1mDebug :\033[m\n" + "Combinaison secrète :\t");
-        for (int i = 0; i < tab.length; i++)
-        {
-            System.out.print(tab[i]+" ");
+        for (int i = 0; i < compareTab.length; i++) {
+            compareTab[i]=Verif.ABSENT;
         }
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < compareTab.length; j++) {
+                if (jTab[i] == tab[j]){
+                    compareTab[i] = Verif.MAL_PLACE;
+                    break;
+                }
+            }
+            if (tab[i] == jTab[i])
+            {
+                compareTab[i] = Verif.BIEN_PLACE;
+            }
+        }
+
+
+        
+
+        
         System.out.print("\nProposition du joueur :\t");
         for (int i = 0; i < jTab.length; i++) {
             System.out.print(jTab[i]+" ");
         }
         System.out.print("\n");
         
+        Mm.afficher(jTab, compareTab);
+
+
+        
         sc.close();
+    }
+    public static void afficher(int[] _jTab, Verif[] _compareTab)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            System.out.print(_compareTab[i].getColor() + _jTab[i] + " ");
+        }
     }
 }
