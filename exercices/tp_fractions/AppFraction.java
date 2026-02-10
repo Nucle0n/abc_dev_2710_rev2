1fff13aa4bd7f22442944156e8425a59e592b598
Nucleon <thibaud.guinchard@gmail.com>
2026-02-10 16:11:14 +0100

    Fin class fraction

--- exercices/tp_fractions/AppFraction.java
+++ exercices/tp_fractions/AppFraction.java
@@ -16,7 +16,20 @@ public class AppFraction {
 
         Fraction fraction = new Fraction(denominateur, numerateur);
 
-        fraction.toString();
+        double resf = (double)fraction.getNumerateur() / (double)fraction.getDenominateur();
+
+        System.out.print("Numérateur : ");
+        numerateur = sc.nextInt();
+        
+        System.out.print("Dénominateur : ");
+        denominateur = sc.nextInt();
+
+        Fraction fraction2 = new Fraction(denominateur, numerateur);
+
+        // double resf2 = (double)fraction.getNumerateur() / (double)fraction.getDenominateur();
+
+        
+
 
         sc.close();
     }
--- exercices/tp_fractions/Fraction.java
+++ exercices/tp_fractions/Fraction.java
@@ -1,5 +1,3 @@
-
-
 /**
  * @author TGuinchard
  * @version 1.0
@@ -10,6 +8,16 @@ public class Fraction {
 	// Attributs
 	private int numerateur;
 	private int denominateur;
+
+	// propriétés
+
+	public int getNumerateur(){
+		return this.numerateur;
+	}
+
+	public int getDenominateur(){
+		return this.denominateur;
+	}
 
 	// Constructeurs
 	public Fraction(){
@@ -23,8 +31,8 @@ public class Fraction {
 	 * @param _numerateur
 	 */
 	public Fraction(int _numerateur){
-		this.denominateur = 1;
 		this.numerateur = _numerateur;
+		this.denominateur = 1;
 	}
 
 	/**
@@ -33,122 +41,165 @@ public class Fraction {
 	 * @param _numerateur
 	 */
 	public Fraction(int _denominateur, int _numerateur){
-		this.denominateur = _denominateur;
+		
+		double res;
 		this.numerateur = _numerateur;
+			
+		try {
+			res = this.numerateur/_denominateur;
+
+			this.denominateur = _denominateur;
+		}
+		catch (ArithmeticException e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage()+" division par zero impossible");
+		}
+		this.reduire();
 	}
 
 	// Méthodes
 
 	public String toString(){
-		return "Fraction : "+numerateur+"/"+denominateur;
+		return "Fraction : " + this.numerateur + "/" + this.denominateur + "\n";
 	}
 
-	private int GetPgcd(){
-		return 0;
+	public void inverse(){
+		int temp;
+		temp = this.numerateur;
+		this.numerateur = this.denominateur;
+		this.denominateur = temp;
+		this.reduire();
 	}
 
-	private void Reduire(){
-
+	public void oppose(){
+		this.numerateur *= (-1);
 	}
 
-	/**
-	 * 
-	 * @param uneFraction
-	 */
-	public boolean Superieur(Fraction uneFraction){
-		return false;
-	}
+	private int getPgcd(){
+		int a = this.numerateur;
+		int b = this.denominateur;
+		int pgcd=-1;
+		if ( a!=0 && b!=0)
+		{
+		if ( a<0 ) a =-a;
+		if ( b<0 ) b =-b;
+		while ( a!=b )
+		if ( a<b )
+		b -= a;
+		else
+		a -=b;
+		pgcd = a;
+		}
+return pgcd;
+}
 
-	/**
-	 * 
-	 * @param uneFraction
-	 */
-	public boolean Inferieur(Fraction uneFraction){
-		return false;
-	}
-
-	/**
-	 * 
-	 * @param uneFraction
-	 */
-	public boolean Egal(Fraction uneFraction){
-		return false;
-	}
-
-	/**
-	 * 
-	 * @param uneFraction
-	 */
-	public Fraction Somme(Fraction uneFraction){
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param uneFraction
-	 */
-	public Fraction Difference(Fraction uneFraction){
-		return null;
+	private void reduire(){
+		int pgcd = this.getPgcd();
+		this.numerateur /= pgcd;
+		this.denominateur /= pgcd;
+		if (this.denominateur < 0){
+			this.numerateur *= -1;
+			this.denominateur *= -1;
+		}
 	}
 
 	/**
 	 * 
 	 * @param uneFraction
 	 */
-	public Fraction Produit(Fraction uneFraction){
-		return null;
+	public Fraction somme(Fraction _uneFraction){
+		int termNum	 = this.numerateur * _uneFraction.getDenominateur() + _uneFraction.getNumerateur() * this.denominateur;
+		int termDeno = this.denominateur * _uneFraction.getDenominateur();
+	
+		Fraction resultat = new Fraction(termNum, termDeno);
+
+		return resultat;
 	}
 
 	/**
-	 * 
+	 *
 	 * @param uneFraction
 	 */
-	public Fraction Quotient(Fraction uneFraction){
-		return null;
+	public Fraction difference(Fraction _uneFraction){
+		
+		Fraction newFraction;
+		_uneFraction.oppose();
+		newFraction = this.somme(_uneFraction);
+		//optionel
+		_uneFraction.oppose();
+		newFraction.reduire();
+
+		return newFraction;
 	}
 
 	/**
-	 * 
+	 *
+	 * @param uneFraction
+	 */
+	public boolean superieur(Fraction _uneFraction){
+
+		double res1 = (double)this.numerateur / (double)this.denominateur;
+		double res2 = (double)_uneFraction.getNumerateur()/(double)_uneFraction.getDenominateur();
+		return  res1 > res2;
+		}
+
+	/**
+	 *
+	 * @param uneFraction
+	 */
+	public boolean inferieur(Fraction _uneFraction){
+
+		double res1 = (double)this.numerateur / (double)this.denominateur;
+		double res2 = (double)_uneFraction.getNumerateur()/(double)_uneFraction.getDenominateur();
+		return  res1 < res2;
+	}
+
+	/**
+	 *
+	 * @param uneFraction
+	 */
+	public boolean egal(Fraction _uneFraction){
+
+		double res1 = (double)this.numerateur / (double)this.denominateur;
+		double res2 = (double)_uneFraction.getNumerateur()/(double)_uneFraction.getDenominateur();
+		return  res1 == res2;
+	}
+
+	/**
+	 *
+	 * @param uneFraction
+	 */
+	public Fraction produit(Fraction _uneFraction){
+		int newNum = this.numerateur * _uneFraction.getNumerateur();
+		int newDen = this.denominateur * _uneFraction.getDenominateur();
+
+		Fraction newFraction = new Fraction(newNum, newDen);
+
+		return newFraction;
+	}
+
+	/**
+	 *
+	 * @param uneFraction
+	 */
+	public Fraction quotient(Fraction _uneFraction){
+
+		_uneFraction.inverse();
+		return this.produit(_uneFraction);
+		
+	}
+
+	/**
+	 *
 	 * @param Exposant
 	 */
-	public Fraction Puissance(int Exposant){
-		return null;
+	public Fraction Puissance(int _Exposant){
+
+		double newNum = Math.pow((double)this.numerateur, _Exposant);
+		double newDen = Math.pow((double)this.denominateur, _Exposant);
+
+		Fraction newFraction = new Fraction((int)newNum, (int)newDen);
+
+		return newFraction;
 	}
-
-	public void Oppose(){
-
-	}
-
-	public void Inverse(){
-
-	}
-
-	public int getNumerateur(){
-		return Numerateur;
-	}
-
-	/**
-	 * 
-	 * @param newVal
-	 */
-	public void setNumerateur(int newVal){
-		Numerateur = newVal;
-	}
-
-	public int getDenominateur(){
-		return Denominateur;
-	}
-
-	/**
-	 * 
-	 * @param newVal
-	 */
-	public void setDenominateur(int newVal){
-		Denominateur = newVal;
-	}
-
-
-
-
-
 }
