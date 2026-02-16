50cc42d7a2c814b872e1a760bac8d9430d3cf32e
Nucleon <thibaud.guinchard@gmail.com>
2026-02-16 20:19:08 +0100

    Mise en forme App Statistiques

--- exercices/statistiques/src/App.java
+++ exercices/statistiques/src/App.java
@@ -12,15 +12,15 @@ public class App {
             System.out.print(element + " ");
         }
 
-        System.out.print("\nTaille de l'arraylist : "+ salaires.size());
+        System.out.print("\n\n\033[1;4;38;2;0;50;225mTaille de l'arraylist :\033[0m "+ salaires.size());
 
         System.out.println("");
 
         moyenne = Methodes.moyenne(salaires);
-        System.out.println("Moyenne : "+ moyenne);
+        System.out.println("\n\033[1;4;38;2;225;225;0mMoyenne :\033[0m "+ moyenne);
 
         mediane = Methodes.mediane(salaires);
-        System.out.println("Mediane : "+ mediane);
+        System.out.println("\n\033[1;4;38;2;225;225;0mMediane :\033[0m "+ mediane);
 
 
     }
