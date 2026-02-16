58dbd7914c5ef9fc90d9403a739216d4466595e2
Nucleon <thibaud.guinchard@gmail.com>
2026-02-16 11:37:25 +0100

    Debut Statistques

---
+++ exercices/statistiques/Methodes.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Methodes {
+    public static void main(String[] args) {
+        // Attributs
+        ArrayList<double> salaires = new ArrayList<double>()
+        salaire.add(2000)
+        for (int i = 0; i < 100; i++) {
+            salaires.add(salaires(i)+50);
+            System.out.print(salaire(i)+ " ");
+        }
+
+
+    }
+}
